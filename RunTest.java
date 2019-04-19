e27a653ab821319856efaebd8b3526c779bbbee3
rustgip <rustgip@vcu.edu>
2019-04-19 16:31:09 -0400

    Add files via upload

---
+++ RunTest.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class RunTest {
+
+	    public static void main(String[] args) throws InterruptedException, IOException
+	      {
+	          for(int i=1; i<=10; i++)
+	              UAVProject.main(args); 
+	      }
+	 }
+	
